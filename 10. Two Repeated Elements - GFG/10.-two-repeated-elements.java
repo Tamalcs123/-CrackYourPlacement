1350693e8e202cdb3a555f78f48a1702bb480466
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-05-17 21:20:21 +0530

    Added solution - LeetHub

---
+++ 10. Two Repeated Elements - GFG/10.-two-repeated-elements.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int t = Integer.parseInt(br.readLine());
+	    for(int i=0;i<t;i++){
+	        int n = Integer.parseInt(br.readLine());
+	        String l = br.readLine();
+    		String[] sarr = l.split(" ");
+    		int[] arr = new int[sarr.length];
+    		for(int i1=0;i1<arr.length;i1++){
+    			arr[i1] = Integer.parseInt(sarr[i1]);
+    		}
+    		
+            Solution obj = new Solution();
+            
+            int[] res = obj.twoRepeated(arr, n);
+            System.out.println(res[0] + " " + res[1]);
+	    }
+	}
+}// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find two repeated elements.
+    public int[] twoRepeated(int arr[], int N)
+    {
+        // Your code here
+       int brr[] = new int[2];
+       int count[] = new int[N+2];
+       int j =0;
+       for(int i =0;i<arr.length;i++)
+       {
+           if(count[arr[i]]==1)
+           {
+               brr[j] = arr[i];
+               j++;
+           }
+           else
+           {
+               count[arr[i]]++;
+           }
+       }
+       return brr;
+     
+    }
+    
+}
