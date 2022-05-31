cb1deecbda356c980c83c4ebb43fe29bcb28a0c9
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-05-31 10:15:33 +0530

    Added solution - LeetHub

--- 16. Kth smallest element - GFG/16.-kth-smallest-element.java
+++ 16. Kth smallest element - GFG/16.-kth-smallest-element.java
@@ -44,10 +44,34 @@ class Solution
     public static int kthSmallest(int arr[], int n, int k)
     {
         // Your code here
-        Arrays.sort(arr);
-        return arr[k-1];
+        int l=0,r=n-1;
+        while(l<=r){
+            int p=partition(arr,l,r);
+            if(p==k-1)
+                return arr[k-1];
+            else if(p>k-1)
+                r=p-1;
+            else
+                l=p+1;
+        }
+        return -1;
     }
-      
+   static int partition(int arr[], int l, int h)
+    {   
+        int pivot=arr[h];
+        int i=l-1;
+        for(int j=l;j<=h-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return i+1;
+    }
     
-
 }
