3cfceb7bb683307d7b3fe1d4cd4c6f26a66bcf38
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-06-05 17:30:42 +0530

    Added solution - LeetHub

---
+++ 12. Numbers containing 1, 2 and 3 - GFG/12.-numbers-containing-1-2-and-3.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.awt.Point; 
+import java.util.Arrays; 
+import java.util.Vector; 
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		findAll();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    Arrays.sort(arr);
+		    boolean flag=false;
+		    for(int x:arr)
+		    {
+		        if(mp.containsKey(x))
+		        {System.out.print(x+" ");flag=true;}
+		    }
+		    
+		    if(flag==false)
+		    System.out.print(-1);
+		    System.out.println();
+		}
+		
+		
+	}
+	
+	
+    static HashMap<Integer,Integer>mp=new HashMap<>();
+
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+//Function to find all the numbers with only 1,2 and 3 in their digits.
+public static void findAll()
+{
+    //Your code here
+     for(int i =0;i<1000001;i++)
+    {
+        if(check(i)==true)
+        {
+        mp.put(i,1);
+        }
+    }
+  
+}
+static boolean check(int n)
+{
+    while(n>0)
+    {
+        int key = n%10;
+        if(key==0 || key>=4)
+        {
+            return false;
+        }
+        n =n/10;
+    }
+    return true;
+}
+  
+      
+
+
+
+
+// { Driver Code Starts.
+
+
+}  // } Driver Code Ends
