41ab73095d120197abb778409e82bee69ffe020a
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-06-20 12:18:05 +0530

    Added solution - LeetHub

--- 19. Check if Linked List is Palindrome - GFG/19.-check-if-linked-list-is-palindrome.java
+++ 19. Check if Linked List is Palindrome - GFG/19.-check-if-linked-list-is-palindrome.java
@@ -99,30 +99,35 @@ class Node
 class Solution
 {
     //Function to check whether the list is palindrome.
-      static Node reverseList(Node head){
-        if(head==null||head.next==null)return head;
-        Node rest_head=reverseList(head.next);
-        Node rest_tail=head.next;
-        rest_tail.next=head;
-        head.next=null;
-        return rest_head;
+     static Node reverse(Node head){
+        Node temp=head;
+        Node prev=null;
+        while(temp!=null){
+            Node next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+        }
+        return prev;
     }
     boolean isPalindrome(Node head) 
     {
         //Your code here
-         if(head==null)return true;
-        Node slow=head,fast=head;
-        while(fast.next!=null&&fast.next.next!=null){
+        if(head==null || head.next==null) return true;
+        Node fast=head;
+        Node slow=head;
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        Node rev=reverseList(slow.next);
-        Node curr=head;
-        while(rev!=null){
-            if(rev.data!=curr.data)
+        slow.next=reverse(slow.next);
+        slow=slow.next;
+        while(slow!=null){
+            if(head.data!=slow.data){
                 return false;
-            rev=rev.next;
-            curr=curr.next;
+            }
+            head=head.next;
+            slow=slow.next;
         }
         return true;
     }    
