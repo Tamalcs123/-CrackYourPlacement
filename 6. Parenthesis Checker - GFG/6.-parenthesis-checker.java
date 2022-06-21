55e6bdf6975a05946a03d0cdb6b8d47e9ac36a69
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-06-21 17:43:10 +0530

    Added solution - LeetHub

---
+++ 6. Parenthesis Checker - GFG/6.-parenthesis-checker.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+   public static boolean matching(char a,char b){
+        return (( a=='(' && b==')' )||( a=='[' && b==']' )||( a=='{' && b=='}' ));
+    }
+    static boolean ispar(String str)
+    {
+        // add your code here
+        Deque<Character> s=new ArrayDeque<>(); 
+        
+        for (int i = 0; i < str.length(); i++)  
+        { 
+            if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{')  
+            {  
+                s.push(str.charAt(i)); 
+            } 
+            else{
+            if (s.isEmpty()==true) 
+                return false;
+            else if(matching(s.peek(),str.charAt(i))==false)
+                return false;
+            else
+                s.pop();
+            }
+        }    
+        return (s.isEmpty()==true); 
+    }
+}
