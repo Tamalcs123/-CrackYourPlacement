e2998cd39ed23b330f89e5096fe07ca67480e6e3
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-06-30 16:20:15 +0530

    Added solution - LeetHub

---
+++ 7. Generate Binary Numbers - GFG/7.-generate-binary-numbers.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        //Taking input using class Scanner 
+        Scanner sc = new Scanner(System.in);
+        
+        //Taking the number of testcases
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            //taking the range
+            int n = sc.nextInt();
+            
+            //creating an ArrayList
+            ArrayList<String> res = new ArrayList<String>();
+            
+            //calling the generate method of class solve
+            //and storing the result in an ArrayList
+            res = new solve().generate(n);
+            
+            //printing all the elements of the ArrayList
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class solve{
+    
+    //Function to generate binary numbers from 1 to N using a queue.
+    static ArrayList<String> generate(int N)
+    {
+        // Your code here
+            ArrayList<String>list=new ArrayList<>();
+            Queue<String>q=new LinkedList<>();
+              q.add("1");
+              for(int i=0;i<N;i++){
+               String curr=q.peek();
+               q.remove();
+               list.add(curr);
+               q.add(curr+"0");
+               q.add(curr+"1");
+          }
+          return list;
+    }
+    
+}
