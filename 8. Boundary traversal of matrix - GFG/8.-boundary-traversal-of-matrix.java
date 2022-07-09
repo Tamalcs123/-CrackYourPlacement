3ed93ad255fa9b6105b7cb42b5681b4270417f95
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-07-09 19:31:38 +0530

    Added solution - LeetHub

--- 8. Boundary traversal of matrix - GFG/8.-boundary-traversal-of-matrix.java
+++ 8. Boundary traversal of matrix - GFG/8.-boundary-traversal-of-matrix.java
@@ -43,38 +43,30 @@ class Solution
     {
         // code here 
           ArrayList<Integer> al = new ArrayList<Integer>();
-       int rowBegin = 0 , rowEnd = n-1;
-       int colBegin = 0 , colEnd = m-1;
-       
-       //Traversing First Row
-       for(int i=colBegin; i<=colEnd; i++)
-       {
-           al.add(matrix[rowBegin][i]);
-       }
-       rowBegin++;
-       
-       //Traversing Last Column
-       for(int i=rowBegin; i<=rowEnd; i++)
-       {
-           al.add(matrix[i][colEnd]);
-       }
-       colEnd--;
-       
-       if(rowBegin > rowEnd || colBegin > colEnd) return al;
-       
-       //Traversing Last Row
-       for(int i=colEnd; i>=colBegin; i--)
-       {
-           al.add(matrix[rowEnd][i]);
-       }
-       rowEnd--;
-       
-       //Traversing First Column
-       for(int i=rowEnd; i>=rowBegin; i--)
-       {
-           al.add(matrix[i][colBegin]);
-       }
-       colBegin++;
-       return al;
+          if(n==1){
+              for(int i=0;i<m;i++){
+                  al.add(matrix[0][i]);
+              }
+          }
+          else if(m==1){
+              for(int i=0;i<n;i++){
+                  al.add(matrix[i][0]);
+              }
+          }
+          else{
+              for(int i=0;i<m;i++){
+                  al.add(matrix[0][i]);
+              }
+              for(int i=1;i<n;i++){
+                  al.add(matrix[i][m-1]);
+              }
+              for(int i=m-2;i>=0;i--){
+                  al.add(matrix[n-1][i]);
+              }
+              for(int i=n-2;i>=1;i--){
+                  al.add(matrix[i][0]);
+              }
+          }
+          return al;
     }
 }
