8f7ddf2e381b2faf8e045d639bedcaedc9fd4b2e
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2022-07-09 19:33:33 +0530

    Added solution - LeetHub

--- 5. Transpose of Matrix - GFG/5.-transpose-of-matrix.java
+++ 5. Transpose of Matrix - GFG/5.-transpose-of-matrix.java
@@ -42,14 +42,12 @@ class Solution
     static void transpose(int matrix[][], int n)
     {
         // code here
-          for(int i=0; i<n; i++){
-           
-           for(int j=0; j<i; j++){
-               
-              int temp = matrix[i][j];
-              matrix[i][j]=matrix[j][i];
-              matrix[j][i]=temp;
-           }
-      }    
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
     }
 }
